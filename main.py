c841cd2cecf15eca2957b8aab0fec6d96e4cc005
Pierrot LeCon <pstmrtem@outlook.com>
2023-05-28 18:46:42 +0200

    feat: hydra config

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
+from pathlib import Path
+
+import hydra
+import torch
+from hydra.utils import to_absolute_path
+from omegaconf import DictConfig, OmegaConf
+
+from src.dataset import ImageDataset
 from src.model.diffusion import DiffusionModel
 
-model = DiffusionModel(
-    image_size=256,
-    n_channels=3,
-    n_tokens=8,
-    hidden_size=256,
-    n_heads=4,
-    ff_size=1024,
-    dropout=0.1,
-    n_layers=6,
-)
 
-model.summary("cpu")
+def init_model(config: DictConfig) -> DiffusionModel:
+    model = DiffusionModel(
+        image_size=config.data.image_size,
+        n_channels=config.data.n_channels,
+        n_tokens=config.model.n_tokens,
+        hidden_size=config.model.hidden_size,
+        n_heads=config.model.n_heads,
+        ff_size=config.model.ff_size,
+        dropout=config.model.dropout,
+        n_layers=config.model.n_layers,
+    )
+    model.summary(config.device)
+    return model
+
+
+def init_dataset(config: DictConfig) -> ImageDataset:
+    dataset = ImageDataset.from_folder(config.data.path, config.data.image_size)
+    return dataset
+
+
+@hydra.main(version_base="1.3", config_path="configs", config_name="default")
+def main(config: DictConfig):
+    config.data.path = Path(to_absolute_path(config.data.path))
+    if config.device == "auto":
+        config.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model = init_model(config)
+    dataset = init_dataset(config)
+
+
+if __name__ == "__main__":
+    # Launch with hydra.
+    main()
---
+++ src/dataset.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset
+from torchvision.io import ImageReadMode, read_image
+from torchvision.transforms.functional import resize
+
+
+class ImageDataset(Dataset):
+    def __init__(self, image_paths: list[Path], image_size: int):
+        self.image_paths = image_paths
+        self.image_size = image_size
+
+    def __len__(self) -> int:
+        return len(self.image_paths)
+
+    def __getitem__(self, index: int) -> torch.Tensor:
+        """Read the image at the given index.
+        The image is normalized to the range [0, 1].
+
+        ---
+        Args:
+            index: The index of the image to read.
+
+        ---
+        Returns:
+            The image at the given index.
+                Shape of [n_channels, images_size, images_size].
+        """
+        image_path = self.image_paths[index]
+        image = read_image(str(image_path), mode=ImageReadMode.RGB)
+        image = image / 255.0
+        image = resize(image, size=[self.image_size, self.image_size])
+        return image
+
+    @classmethod
+    def from_folder(cls, folder_path: Path, image_size: int) -> "ImageDataset":
+        image_paths = [path for path in folder_path.iterdir() if path.is_file()]
+        return cls(image_paths, image_size)
